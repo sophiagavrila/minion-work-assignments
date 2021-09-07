31a4302ccee0835642568bb3c3a67af1f00cbdd3
sophiagavrila <msophiagavrila@gmail.com>
2021-09-06 23:10:11 -0400

    add 10 JUnit tests

--- src/main/java/com/revature/Solution.java
+++ src/main/java/com/revature/Solution.java
@@ -1,9 +1,17 @@
 package com.revature;
 
 public class Solution {
-	
+
 	/**
-	 * Create the static method called solution below...
+	 * Implement the mehtod below, then right click on
+	 * SolutionTests.java within src/test/java/ -> com.revature to test your code
+	 * before committing and pushing to your repository.
 	 */
+	
+	public static int[] solution(int[] data, int n) {
+		
+		
+		return null;
+	}
 
 }
