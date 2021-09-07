3b9f682bdd461e00ac0916fc6c98c4e09270a952
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-09-07 00:33:39 -0400

    Update Solution.java

--- src/main/java/com/revature/Solution.java
+++ src/main/java/com/revature/Solution.java
@@ -3,7 +3,7 @@ package com.revature;
 public class Solution {
 
 	/**
-	 * Implement the mehtod below, then right click on
+	 * Implement the method below, then right click on
 	 * SolutionTests.java within src/test/java/ -> com.revature to test your code
 	 * before committing and pushing to your repository.
 	 */
